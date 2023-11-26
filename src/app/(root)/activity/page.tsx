e22962723bfcf9546bcbb6cee2b5e07cf53d821c
Ivan Butakov <ivanbutakov000@gmail.com>
2023-11-26 14:21:05 +0500

    Create activity page

---
+++ src/app/(root)/activity/page.tsx
@@ -0,0 +1,48 @@
+import { FetchUser, getActivity } from '@/lib/actions/user.actions'
+import { currentUser } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+
+const Page = async () => {
+	const user = await currentUser()
+	if (!user) return null
+
+	const userInfo = await FetchUser(user.id)
+	if (!userInfo?.onboarded) redirect('/onboarding')
+
+	// get activity
+	const activity = await getActivity(userInfo._id)
+	console.log(activity)
+	return (
+		<section>
+			<h1 className='head-text mb-10'>Activity</h1>
+			<div className='flex flex-col gap-5'>
+				{activity.length === 0 ? (
+					<p className='text-light-1'>No activity found</p>
+				) : (
+					<>
+						{activity.map(activity => (
+							<Link key={activity._id} href={`/thread/${activity.parentId}`}>
+								<article className='flex gap-3 items-center rounded-md bg-dark-2 px-5 py-3 '>
+									<Image
+										src={activity.author.image}
+										alt='user image'
+										width={20}
+										height={20}
+										className='rounded-full object-cover'
+									/>
+									<p className='text-white'>
+										<span className='text-primary'>{activity.author.name}</span>{' '}
+										replied to your thread
+									</p>
+								</article>
+							</Link>
+						))}
+					</>
+				)}
+			</div>
+		</section>
+	)
+}
+export default Page
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -147,3 +147,31 @@ export const fetchUsers = async ({
 		throw new Error(`Failed to fetch users: ${err.message}`)
 	}
 }
+
+export const getActivity = async (userId: string) => {
+	try {
+		connectToDatabase()
+
+		// Find all threads created by the user
+		const userThreads = await Thread.find({ author: userId })
+
+		// Collect all the child thread ids (replies) from the 'children' field of each user thread
+		const childThreadIds = userThreads.reduce((acc, userThread) => {
+			return acc.concat(userThread.children)
+		}, [])
+
+		// Find and return the child threads (replies) excluding the ones created by the same user
+		const replies = await Thread.find({
+			_id: { $in: childThreadIds },
+			author: { $ne: userId }, // Exclude threads authored by the same user
+		}).populate({
+			path: 'author',
+			model: User,
+			select: 'name image _id',
+		})
+
+		return replies
+	} catch (err: any) {
+		throw new Error(`Failed to fetch activity: ${err.message}`)
+	}
+}
